a6f822361e4dc193eaf8e660a8b39ad673112b41
BehnamHedayat <103999348+BehnamHedayat@users.noreply.github.com>
2023-05-02 10:09:40 +0330

    first practic

---
+++ ThirdAngle.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+contract thiredAngle {
+
+    function otherAngle(int angl1, int angl2) public pure returns (int) {
+        int a1 = angl1;
+        int a2 = angl2;
+        int a3;
+        a3 = 180 - a1 - a2;
+        return a3;
+    }
+    
+}
