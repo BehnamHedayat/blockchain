d06d4aad202ba4dbb24abc8cde4ec84591072155
BehnamHedayat <103999348+BehnamHedayat@users.noreply.github.com>
2023-05-02 11:32:13 +0330

    Update ThirdAngle.sol
    second commit

--- ThirdAngle.sol
+++ ThirdAngle.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 contract thiredAngle {
+//this function get 2 angle of triangle and then return another angle.
 
     function otherAngle(int angl1, int angl2) public pure returns (int) {
         int a1 = angl1;
